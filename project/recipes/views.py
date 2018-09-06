69ece3335b18f0fd28856a5cbbf6951903775cbf
Thiery Louison <thiery.louison@gmail.com>
2018-09-06 17:12:21 +0400

    add templates

--- project/recipes/views.py
+++ project/recipes/views.py
@@ -1,11 +1,12 @@
 # project/recipes/views.py
 from flask import render_template, Blueprint
-
+from project.models import Recipe
 # template config
 recipes_blueprint = Blueprint('recipes', __name__,
                               template_folder='templates')
 
 
 @recipes_blueprint.route('/')
-def indec():
-    return render_template('index.html')
+def index():
+    all_recipes = Recipe.query.all()
+    return render_template('recipes.html', recipes=all_recipes)
