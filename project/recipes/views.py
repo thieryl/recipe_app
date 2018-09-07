91093d94cc9222edb833e12307f873053e6a1fda
thiery louison <thiery.louison@redboxdigital.com>
2018-09-07 16:33:53 +0400

    Finish adding templates

--- project/recipes/views.py
+++ project/recipes/views.py
@@ -1,12 +1,44 @@
 # project/recipes/views.py
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request, redirect, url_for, flash
+from project import db
 from project.models import Recipe
+from .forms import AddRecipeForm
+
+
 # template config
 recipes_blueprint = Blueprint('recipes', __name__,
                               template_folder='templates')
+
+
+def flash_errors(form):
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash(u"Error in the %s field - %s" % (
+                getattr(form, field).label.text,
+                error
+            ), 'info')
 
 
 @recipes_blueprint.route('/')
 def index():
     all_recipes = Recipe.query.all()
     return render_template('recipes.html', recipes=all_recipes)
+
+
+@recipes_blueprint.route('/add', methods=['GET', 'POST'])
+def add_recipe():
+    form = AddRecipeForm(request.form)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            new_recipe = Recipe(form.recipe_title.data,
+                                form.recipe_description.data)
+            db.session.add(new_recipe)
+            db.session.commit()
+            flash('New recipe, {}, added!'.format(
+                new_recipe.recipe_title), 'success')
+            return redirect(url_for('recipes.index'))
+        else:
+            flash_errors(form)
+            flash('ERROR! Recipe was not added.', 'error')
+
+    return render_template('add_recipe.html', form=form)
--- project/tests/test_recipes.py
+++ project/tests/test_recipes.py
@@ -1,6 +1,9 @@
 # project/test/test_recipes.py
+import os
 import unittest
-from project import app
+from project import app, db
+
+TEST_DB = 'test.db'
 
 
 class ProjecTests(unittest.TestCase):
@@ -10,8 +13,12 @@ class ProjecTests(unittest.TestCase):
 
     def setUp(self):
         app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+            os.path.join(app.config['BASEDIR'], TEST_DB)
         app.config['DEBUG'] = False
         self.app = app.test_client()
+        db.create_all()
 
         self.assertEquals(app.debug, False)
 
@@ -27,6 +34,37 @@ class ProjecTests(unittest.TestCase):
         self.assertIn(b'Lunch Recipes', response.data)
         self.assertIn(b'Dinner Recipes', response.data)
         self.assertIn(b'Dessert Recipes', response.data)
+
+    # Main page query result
+    def test_main_page_query_results(self):
+        response = self.app.get('/add', follow_redirects=True)
+        self.assertIn(b'Add a New Recipes', response.data)
+
+    # add Valid recipe
+    def test_add_recipe(self):
+        response = self.app.post(
+            '/add',
+            data=dict(recipe_title='Hamburgers',
+                      recipe_description='Delicious Hamburgers with pretzel rolls'),
+            follow_redirects=True)
+        self.assertIn(b'New recipe, Hamburgers, added!', response.data)
+
+    def test_add_recipes(self):
+        response = self.app.post(
+            '/add',
+            data=dict(recipe_title='Hamburgers',
+                      recipe_description='Delicious hamburger with pretzel rolls'),
+            follow_redirects=True)
+        self.assertIn(b'New recipe, Hamburgers, added!', response.data)
+
+    # add Invalid recipe
+
+    def test_add_invalid_recipe(self):
+        response = self.app.post('/add',
+                                 data=dict(recipe_title='',
+                                           recipe_description='Delicious Hamburgers with pretzel rolls'),
+                                 follow_redirects=True)
+        self.assertIn(b'This field is required.', response.data)
 
 
 if __name__ == "__main__":
