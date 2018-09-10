aca256c6eb64272523eeb60d209ab3b2a6726332
thiery louison <thiery.louison@redboxdigital.com>
2018-09-10 09:13:43 +0400

    finish add forms

--- project/users/views.py
+++ project/users/views.py
@@ -1,6 +1,8 @@
 # project/users/views.py
-from flask import render_template, Blueprint
-
+from flask import render_template, Blueprint, redirect, request, url_for, flash
+from project import db
+from project.models import User
+from .forms import RegisterForm
 # Blueprint config
 
 users_blueprint = Blueprint('users', __name__,
@@ -10,3 +12,22 @@ users_blueprint = Blueprint('users', __name__,
 @users_blueprint.route('/login')
 def login():
     return render_template('login.html')
+
+
+@users_blueprint.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            try:
+                new_user = User(form.email.data, form.password.data)
+                new_user.authenticated = True
+                db.session.add(new_user)
+                db.session.commit()
+                flash('Thanks for registering', 'success')
+                return redirect(url_for('recipes.index'))
+            except IntegrityError:
+                db.session.rollback()
+                flash('ERROR! Email ({}) alreadu exist.',
+                      format(form.emai.data), 'error')
+        return render_template('register.html', form=form)
